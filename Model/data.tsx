7107bb82f97f47587b82300fcc1bcf8be3632fe8
halino2710 <kemokaihamed2710@gmail.com>
2023-03-31 15:41:02 +0000

    Added Model (data) component

---
+++ Model/data.tsx
@@ -0,0 +1,49 @@
+export const servicesData = [
+  {
+    image: require("../assets/images/webapp.png"),
+    title: "Website & Mobile Applications",
+  },
+  {
+    image: require("../assets/images/dashboard.png"),
+    title: "Dashboard & Analytics",
+  },
+  {
+    image: require("../assets/images/e-commerce.png"),
+    title: "E-Shopping",
+  },
+  {
+    image: require("../assets/images/payment.png"),
+    title: "Online Payments",
+  },
+  {
+    image: require("../assets/images/library.png"),
+    title: "Digital Library",
+  },
+  {
+    image: require("../assets/images/buss.png"),
+    title: "Bus Fleet Management",
+  },
+];
+
+export const memeberData = [
+  {
+    image: require("../assets/images/isaac.png"),
+    name: "Isaac Johnson",
+    position: "Chief Executive Officer",
+  },
+  {
+    image: require("../assets/images/shelp_profile.png"),
+    name: "Mohamed Shelpidy Kamara",
+    position: "Chief Operating Officer",
+  },
+  {
+    image: require("../assets/images/Afa-pearl.png"),
+    name: "Afanwi Pearl",
+    position: "Lead UI/UX Designer",
+  },
+  {
+    image: require("../assets/images/hamed.png"),
+    name: "Hamed Kemokai",
+    position: "Leading Mobile Developer",
+  },
+];
