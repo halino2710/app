45e18a4335a8751ca1e678826d51cf3bc8b7d670
halino2710 <kemokaihamed2710@gmail.com>
2023-03-31 18:34:48 +0000

    Added members data to Model (data) component

--- Model/data.tsx
+++ Model/data.tsx
@@ -1,47 +1,70 @@
 export const servicesData = [
   {
+    id: 1,
     image: require("../assets/images/webapp.png"),
     title: "Website & Mobile Applications",
+    content:
+      "At SchoolAll, we offer cutting-edge website and mobile application development services for both Android and iOS devices. Our solutions feature modern, elegant user interface designs that are optimized to enhance user engagement and provide an exceptional user experience.",
   },
+
   {
+    id: 2,
     image: require("../assets/images/dashboard.png"),
     title: "Dashboard & Analytics",
+    content:
+      "At SchoolAll, our Dashboard & Analytics software service provides you with real-time metrics and analysis to help you manage your school resources and data effortlessly. Our powerful analytics system generates meaningful statistics and a comprehensive summary of all activities within your school. With our Dashboard & Analytics service, you can make informed decisions and take timely actions to improve the overall efficiency and effectiveness of your academic institution.",
   },
   {
+    id: 3,
     image: require("../assets/images/e-commerce.png"),
     title: "E-Shopping",
+    content:
+      "SchoolAll's E-Shopping platform is the ultimate all-in-one solution for all your school-related shopping needs. With an easy-to-use interface, parents and guardians can browse and shop for school materials just like any e-commerce website. Our platform streamlines the shopping experience, providing a hassle-free and convenient way to access all your school products in one place.",
   },
   {
+    id: 4,
     image: require("../assets/images/payment.png"),
     title: "Online Payments",
+    content:
+      "Our online payment service at SchoolAll allows you to easily pay for school tuition fees and additional charges using a variety of integrated payment platforms, including mobile money and bank transfers. With near-instant payment receipts, you can keep track of your payments and ensure that all transactions are processed quickly and securely.",
   },
   {
+    id: 5,
     image: require("../assets/images/library.png"),
     title: "Digital Library",
+    content:
+      "Our Digital Library service at SchoolAll makes it easy for academic institutions to manage and provide access to a wide range of resources, including books, lesson notes, past questions and more. With our easy-to-use platform, students and teachers can quickly find the resources they need without any hassle or frustration.",
   },
   {
+    id: 6,
     image: require("../assets/images/buss.png"),
     title: "Bus Fleet Management",
+    content:
+      "BFM allows: - to create fleet schedules with easy. - to sync schedules with your calendars apps. - receive notification on arrivals and departures.",
   },
 ];
 
 export const memeberData = [
   {
+    id: 1,
     image: require("../assets/images/isaac.png"),
     name: "Isaac Johnson",
     position: "Chief Executive Officer",
   },
   {
+    id: 2,
     image: require("../assets/images/shelp_profile.png"),
     name: "Mohamed Shelpidy Kamara",
     position: "Chief Operating Officer",
   },
   {
+    id: 3,
     image: require("../assets/images/Afa-pearl.png"),
     name: "Afanwi Pearl",
     position: "Lead UI/UX Designer",
   },
   {
+    id: 4,
     image: require("../assets/images/hamed.png"),
     name: "Hamed Kemokai",
     position: "Leading Mobile Developer",
