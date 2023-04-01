ecd99272828049814f3b50bdd1717111dab7c464
halino2710 <kemokaihamed2710@gmail.com>
2023-04-01 18:12:39 +0000

    Refactored Home component

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,76 +1,25 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ScrollView,
-  StatusBar,
-  Animated,
-} from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-      >
+      <View style={styles.logoContainer}>
         <Image
           source={require("../assets/images/logo.png")}
-          style={{ width: 25, height: 45, marginLeft: 20 }}
+          style={styles.logo}
         />
-        <Text
-          style={{
-            fontSize: 30,
-            fontWeight: "bold",
-            color: "white",
-            marginLeft: 10,
-          }}
-        >
-          School
-        </Text>
-        <Text
-          style={{
-            fontSize: 30,
-            color: "#A15CFF",
-            fontWeight: "bold",
-          }}
-        >
-          All
-        </Text>
+        <Text style={styles.logoSchool}>School</Text>
+        <Text style={styles.logoAll}>All</Text>
       </View>
       <View style={styles.imageContainer}>
         <Image
           source={require("../assets/images/banner.png")}
-          style={styles.logo}
+          style={styles.image}
         />
       </View>
-      <Text
-        style={{
-          color: "white",
-          opacity: 1,
-          fontWeight: "bold",
-          fontSize: 30,
-          lineHeight: 40,
-          textAlign: "justify",
-          marginHorizontal: 20,
-          marginTop: 20,
-        }}
-      >
-        We offer digital services to schools
-      </Text>
-      <Text
-        style={{
-          textAlign: "justify",
-          color: "grey",
-          marginVertical: 8,
-          marginHorizontal: 20,
-          fontSize: 20,
-        }}
-      >
+      <Text style={styles.introMain}>We offer digital services to schools</Text>
+      <Text style={styles.introSub}>
         SchoolAll Company Here to Digitize Education
       </Text>
     </View>
@@ -84,7 +33,30 @@ const styles = StyleSheet.create({
     backgroundColor: "#21005D",
     width: "100%",
     height: "100%",
+  },
+
+  logoContainer: {
+    flexDirection: "row",
     alignItems: "center",
+  },
+
+  logo: {
+    width: 25,
+    height: 45,
+    marginLeft: 20,
+  },
+
+  logoSchool: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+    marginLeft: 10,
+  },
+
+  logoAll: {
+    fontSize: 30,
+    color: "#A15CFF",
+    fontWeight: "bold",
   },
 
   imageContainer: {
@@ -93,8 +65,27 @@ const styles = StyleSheet.create({
     height: "60%",
   },
 
-  logo: {
+  image: {
     width: "95%",
     height: "100%",
+  },
+
+  introMain: {
+    color: "white",
+    opacity: 1,
+    fontWeight: "bold",
+    fontSize: 30,
+    lineHeight: 40,
+    textAlign: "justify",
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+
+  introSub: {
+    textAlign: "justify",
+    color: "grey",
+    marginVertical: 8,
+    marginHorizontal: 20,
+    fontSize: 20,
   },
 });
